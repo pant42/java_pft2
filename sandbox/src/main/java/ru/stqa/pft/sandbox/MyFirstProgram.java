e63a82df06b39bb72388b1a71fcf8b7b1890df2b
pant42 <pantin_42@inbox.ru>
2022-03-08 23:25:39 +0300

    Цельная функция по выводу надписи разбита на несколько и добавлена новая функция
    Функция по вычислению площади квадрата.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,26 @@
 package ru.stqa.pft.sandbox;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("everybody everywhere");
 
-        System.out.println("Hello, world!");
-    }
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + "= " + area(l));
+
+  }
+  public static void hello (String somebody) {
+
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
 }
