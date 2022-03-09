3a7766a3d6b0f2d40163402326005de0be530fba
pant42 <pantin_42@inbox.ru>
2022-03-09 15:16:07 +0300

    distance в классе Point
    Перенос функции distance из класса с main-функцией в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
@@ -5,10 +5,7 @@ public class PointRun {
     Point p1 = new Point(5, 6);
     Point p2 = new Point(9, 10);
 
-    System.out.println("Расстояние = " + distance(p1, p2));
+    System.out.println("Расстояние = " + Point.distance(p1, p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
 }
