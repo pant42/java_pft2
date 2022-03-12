2ac4d7eb27b4337c39e7d36ffa3a6fce43870368
pant42 <pantin_42@inbox.ru>
2022-03-13 01:50:27 +0300

    Исправление ДЗ-2
    расчет расстояния distance выполняется теперь в классе Point, который описывает параметры объекта "Точка", а так же расчет выполняется в исполняемом классе PointRun (результат: два раза расчет выполняется, два результата отработки программы)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
@@ -6,6 +6,10 @@ public class PointRun {
     Point p2 = new Point(9, 10);
 
     System.out.println("Расстояние = " + Point.distance(p1, p2));
+    System.out.println("Расстояние = " + distance(p1, p2));
   }
 
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  }
 }
