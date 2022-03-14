ae3bfb4f135aa6d789a126df51d51328877b7889
pant42 <pantin_42@inbox.ru>
2022-03-14 20:45:11 +0300

    ДЗ-3 Новые тесты
    Добавлены три теста:
    testDistance2 - на статический метод
    testDistance3, 4 - на не статический метод

--- sandbox/src/test/java/ru/stqa/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/sandbox/PointTest.java
@@ -1,19 +1,39 @@
 package ru.stqa.sandbox;
 
-
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.sandbox.Point;
 
 public class PointTest {
- @Test
-  public void testDistance () {
+  @Test
+  public void testDistance() {
 
-    Point p1= new Point(0,3);
-    Point p2= new Point(4,0);
+    Point p1 = new Point(0, 3);
+    Point p2 = new Point(4, 0);
+    Assert.assertEquals(Point.distance(p1, p2), 5.0);
+  }
 
-   Assert.assertEquals( Point.distance (p1, p2),5.0);
+  @Test
+  public void testDistance2() {
+
+    Point p1 = new Point(0, 6);
+    Point p2 = new Point(8, 0);
+    Assert.assertEquals(Point.distance(p1, p2), 10.0);
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(0, 3);
+    Point p2 = new Point(4, 0);
+    Assert.assertEquals(p2.distanceM(p1), 5);
+  }
+
+  @Test
+  public void testDistance4() {
+    Point p1 = new Point(1, 7);
+    Point p2 = new Point(9, 1);
+    Assert.assertEquals(p2.distanceM(p1), 10);
   }
 }
+
 
