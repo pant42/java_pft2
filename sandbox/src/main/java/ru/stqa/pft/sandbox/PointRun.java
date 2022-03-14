53c2925e25b8963800f7c1ea2ac35c019358a0f0
pant42 <pantin_42@inbox.ru>
2022-03-14 03:09:19 +0300

    ДЗ-2 создан метод distanceM

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
@@ -6,10 +6,8 @@ public class PointRun {
     Point p2 = new Point(9, 10);
 
     System.out.println("Расстояние = " + Point.distance(p1, p2));
-    System.out.println("Расстояние = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println("Расстояние = " + p2.distanceM(p1));
   }
 }
+
+
