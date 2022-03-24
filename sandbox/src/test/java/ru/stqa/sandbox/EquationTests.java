2352b99869b3eb6ac47a1fb28798eae4c36d2167
pant42 <pantin_42@inbox.ru>
2022-03-24 22:48:09 +0300

    Улучшена формула выяснения, что за уравнение перед нами. полная и не полная формы if/else

--- sandbox/src/test/java/ru/stqa/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/sandbox/EquationTests.java
@@ -7,20 +7,38 @@ import ru.stqa.pft.sandbox.Equation;
 public class EquationTests {
 
   @Test
-  public void test0() {
+  public void test1() {
     Equation e = new Equation(1, 1, 1);
     Assert.assertEquals(e.rootNumber(), 0);
   }
 
   @Test
-  public void test1() {
+  public void test2() {
     Equation e = new Equation(1, 2, 1);
     Assert.assertEquals(e.rootNumber(), 1);
   }
 
   @Test
-  public void test2() {
+  public void test3() {
     Equation e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2);
+  }
+
+  @Test
+  public void test4LinearN1() {
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void test5ConstantN0() {
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void test6AllZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
   }
 }
