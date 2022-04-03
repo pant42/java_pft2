6183279ebdbdcd26c0ec00367151a0b78ae745fc
pant42 <pantin_42@inbox.ru>
2022-04-03 20:03:39 +0300

    3.9 Добавлена проверка нахождения на странице Groups/ Home перед переходом в  раздел

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -11,10 +11,18 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void returnToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+    return;
+    }
     click(By.linkText("groups"));
   }
 
   public void returnToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
