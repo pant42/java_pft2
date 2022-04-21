47b09b7da34ad05b1f2890d95159982369fa9011
pant42 <pantin_42@inbox.ru>
2022-04-21 18:43:50 +0300

    GroupCreationTest формирование списка элементов. id->int

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -69,10 +69,10 @@ public class GroupHelper extends HelperBase {
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
 
-      String id = element.findElement(By.tagName("input")).getAttribute("value");
+      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       String name = element.getText();
 
-      GroupData group = new GroupData(id, name, null, null);
+      GroupData group = new GroupData(id,name,null,null);
       groups.add(group);
     }
 
