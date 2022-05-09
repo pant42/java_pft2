1241282b395ae0c13f74483d7b9db663cae1793d
pant42 <pantin_42@inbox.ru>
2022-05-09 21:37:58 +0300

    Метод обратных проверок

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -138,15 +138,14 @@ public class ContactHelper extends HelperBase {
       String lastname = cells.get(1).getText();
       String firstname = cells.get(2).getText();
 
-      String[] phones = cells.get(5).getText().split("\n");
+      String allPhones = cells.get(5).getText();
 
       contacts.add(new ContactData().
               withId(id).
               withFirstname(firstname).
               withLastname(lastname).
-              withHomePhone(phones[0]).
-              withMobilePhone(phones[1]).
-              withWorkPhone(phones[2])
+              withAllPhones(allPhones)
+
       );
     }
     return contacts;
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -6,6 +6,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,13 +34,18 @@ public class ContactPhoneTest extends TestBase{
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().InfoFromEditForm(contact);
 
-    assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
-    assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
-    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+    assertThat(contact.getAllPhones(), equalTo(margePhones(contactInfoFromEditForm)));
+  }
+
+  private String margePhones(ContactData contact) {
+    return  Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(),contact.getWorkPhone())
+            .stream().filter((s)-> !s.equals(""))
+            .map(ContactPhoneTest::cleaned)
+            .collect(Collectors.joining("\n"));
 
   }
 
-  public String cleaned(String phone){
+  public static String cleaned(String phone){
     return phone.replaceAll("\\s","").replaceAll("[-()]","");
   }
 }
