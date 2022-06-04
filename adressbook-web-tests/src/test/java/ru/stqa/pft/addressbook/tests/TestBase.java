985ea89f8700311fa36ad0e85b26791b21b168f4
pant42 <pantin_42@inbox.ru>
2022-06-04 14:20:43 +0300

    7.2 ORM + logback

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class TestBase {
+
+  Logger logger = (Logger) LoggerFactory.getLogger(TestBase.class);
+
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -21,5 +31,16 @@ public class TestBase {
     app.logout();
     app.stop();
   }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test"  + m.getName());
+  }
+
 
 }
