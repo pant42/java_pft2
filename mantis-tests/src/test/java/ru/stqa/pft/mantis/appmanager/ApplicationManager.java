ee9dc6d13659e35deb859244e0ba8d4dfe8edfce
pant42 <pantin_42@inbox.ru>
2022-06-12 16:14:36 +0300

    8.4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,16 +9,15 @@ import org.openqa.selenium.remote.BrowserType;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  WebDriver wd;
-
-  JavascriptExecutor js;
+  private WebDriver wd;
+  private final Properties properties;
 
   private String browser;
-  private final Properties properties;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -30,36 +27,43 @@ public class ApplicationManager {
   public void init() throws IOException {
 
     String target = System.getProperty("target", "local");
-
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    js = (JavascriptExecutor) wd;
-
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  public  HttpSession newSession(){
+  public HttpSession newSession() {
     return new HttpSession(this);
   }
 
   public Object getProperty(String key) {
     return properties.getProperty(key);
   }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
+  }
+
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -6,7 +6,7 @@ public class HttpSession {
   private CloseableHttpClient httpclient;
   private ApplicationManager app;
 
-  public HttpSession(ApplicationManager app){
+  public HttpSession(ApplicationManager app) {
     this.app = app;
     httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
   }
@@ -14,20 +14,20 @@ public class HttpSession {
   public boolean login(String username, String password) throws IOException {
     HttpPost post = new HttpPost(app.getProperty("web.baseURL") + "/login.php");
     List<NameValuePair> params = new ArrayList<NameValuePair>();
-    params.add(new BasicNameValuePair("username",username));
-    params.add(new BasicNameValuePair("password",password));
-    params.add(new BasicNameValuePair("secure_session","on"));
-    params.add(new BasicNameValuePair("return","index.php"));
+    params.add(new BasicNameValuePair("username", username));
+    params.add(new BasicNameValuePair("password", password));
+    params.add(new BasicNameValuePair("secure_session", "on"));
+    params.add(new BasicNameValuePair("return", "index.php"));
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = getTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span>",username));
+    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 
   private String getTextFrom(CloseableHttpResponse response) throws IOException {
-    try{
+    try {
       return EntityUtils.toString(response.getEntity());
-    }finally {
+    } finally {
       response.close();
     }
   }
@@ -36,7 +36,7 @@ public class HttpSession {
     HttpGet get = new HttpGet(app.getProperty("web.baseURL") + "/index.php");
     CloseableHttpResponse response = httpclient.execute(get);
     String body = getTextFrom(response);
-    return  body.contains(String.format("<span class=\"italic\">%s</span>",username));
+    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 
 }
