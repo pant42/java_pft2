3f8ad7d69db17cf636eee4abfa8edb5813700067
pant42 <pantin_42@inbox.ru>
2022-06-14 02:45:11 +0300

    8.7. Получение почты на внешний почтовый сервер James (assertTrue(app.newSession().login(user, password)); = false ERROR

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -14,7 +12,7 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
+  // @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -25,10 +23,19 @@ public class RegistrationTests extends TestBase {
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
     String password = "password";
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
+
+    app.james().createUser(user, password);
 
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+
+    //---Либо встроенный почтовый сервис, либо внешний (James):
+
+    //---Встроенный почтовый сервис:
+    // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+
+    //---внешний почтовый сервис (James):
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
 
     String confirmationLink = findConfirmationLink(mailMessages, email);
 
@@ -45,7 +52,7 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod(alwaysRun = true)
+  //@AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
